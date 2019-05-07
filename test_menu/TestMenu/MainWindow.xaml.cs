ec84cb321395083e6d8c8d54856d947c0578c204
bluecatff <544088192@qq.com>
2019-05-07 23:17:24 +0800

    用preview系列函数 handle 捕获事件 阻止系统按照默认的事件关闭menuitem和处理

---
+++ test_menu/TestMenu/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void test()
+        {
+            MenuItem bb = chirdb;
+            
+        }
+
+        private void childa_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+        }
+        private void zhiding_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // TODO: Add event handler implementation here.
+        }
+
+        /// <summary>
+        /// 拦截系统事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void childa_PreviewMouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+            MenuItem item = sender as MenuItem;
+            item.IsChecked = !item.IsChecked;
+            e.Handled = true;
+        }
+    }
+}
