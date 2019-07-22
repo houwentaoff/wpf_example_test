ba00b5232f26233a8a9d3fe5d22ea92906f0d390
bluecatff <544088192@qq.com>
2019-07-22 23:55:41 +0800

    使用微软的语音播报api（声音太像机器人不好听）

---
+++ test_voice/test_voice/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Speech.Synthesis;
+
+namespace test_voice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // TODO: Add event handler implementation here.
+            SpeechSynthesizer speech = new SpeechSynthesizer();
+            string str = "亲爱的用户，您好，这是一个语音合成示例，感谢您对科大讯飞语音技术的支持！科大讯飞是亚太地区最大的语音上市公司，股票代码：002230"; //合成文本
+            /* 用户登录 */
+            speech.Rate = 0;//语速为正常语速
+            speech.SelectVoice("Microsoft Lili");//设置播音员（中文）
+            speech.Volume = 100;//音量为最大音量
+            speech.SpeakAsync(str);//语音播报
+        }
+    }
+}
